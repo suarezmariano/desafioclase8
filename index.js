83fb7e656ed8410923d21514fbc8f2fa5bb83308
suarezmariano <marianosuarez@hotmail.com>
2022-03-10 20:01:29 -0300

    creado de index.html

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
-
 const router = express.Router();
 
+//SERVER
 const port = 8080;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
-
 const server = app.listen(port, () => {
   console.log('servidor corriendo en el puerto: ' + port);
 });
 
 server.on('error', (error) => console.log(`hubo un error ${error}`));
+
+app.use('/', router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const productos = [];
+
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
