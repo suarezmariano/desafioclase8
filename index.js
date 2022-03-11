f202893911a0b72f70d51bb38eee54b3ed0a246b
suarezmariano <marianosuarez@hotmail.com>
2022-03-10 23:43:50 -0300

    agregado de route separada

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
 
-const Contenedor = require('./Contenedor');
-const contenedor = new Contenedor();
+const router = require('./routes/router');
+app.use('/api/productos', router);
 
 //SERVER
 const port = 8080;
 const server = app.listen(port, () => {
   console.log('servidor corriendo en el puerto: ' + port);
 });
-
 server.on('error', (error) => console.log(`hubo un error ${error}`));
 
-app.use('/api/productos', router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-const productos = [];
-
-router.get('/new', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-router.get('/', (req, res) => {
-  const productos = contenedor.showAll();
-  res.json(productos);
-});
---
+++ routes/router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const productos = [];
+
+router.get('/', (req, res) => {
+  res.json(productos);
+});
+
+router.post('/', (req, res) => {
+  let newProduct = {};
+  newProduct.id = productos.length + 1;
+  newProduct.title = req.body.title;
+  newProduct.price = req.body.price;
+  newProduct.image = req.file.image;
+  productos.push(newProduct);
+  res.json(newProduct);
+});
+
+module.exports = router;
