543b4fce6d5b837c4c4c18fe4dcafaee4fecdbf9
suarezmariano <marianosuarez@hotmail.com>
2022-03-17 22:48:00 -0300

    agregado de getbyid y delete

--- routes/router.js
+++ routes/router.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const productos = [];
-
+let productos = [];
+class Product {
+  constructor(id, title, price, thumbnail) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.thumbnail = thumbnail;
+  }
+}
 router.get('/', (req, res) => {
   res.json(productos);
 });
 
 router.post('/', (req, res) => {
-  let newProduct;
+  let newProduct = new Product();
   newProduct.id = productos.length + 1;
   newProduct.title = req.body.title;
   newProduct.price = req.body.price;
@@ -16,6 +23,35 @@ router.post('/', (req, res) => {
   console.log(newProduct);
   productos.push(newProduct);
   res.json(newProduct);
+});
+
+router.get('/:id', (req, res) => {
+  const product = productos.filter((product) => product.id == req.params.id);
+  if (product != '') {
+    res.json(product);
+  } else {
+    res.json('Producto no encontrado');
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  let id = productos.filter((product) => product.id == req.params.id);
+  if (id != '') {
+    let products = productos.filter((product) => product.id != req.params.id);
+    productos = products;
+    res.json('Producto eliminado');
+  } else {
+    res.json('Producto no encontrado');
+  }
+});
+
+router.put('/:id', (req, res) => {
+  const product = productos.filter((product) => product.id == req.params.id);
+  if (product != '') {
+    res.json(product);
+  } else {
+    res.json('Producto no encontrado');
+  }
 });
 
 module.exports = router;
